22ce5530bb4bce417d9473b010d9f8fecb3307c7
刘金涛 <tao2500594037@qq.com>
2023-03-13 19:54:07 +0800

    20230313

--- js/leetCode-1275.js
+++ js/leetCode-1275.js
@@ -1,6 +1,6 @@
-tictactoe([[0,0],[2,0],[1,1],[2,1],[2,2]]);
+tictactoe([[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]);
 function tictactoe (moves) {
-  let arr = Array.from(Array(3), () => new Array(3));
+  let arr = Array.from(Array(3), () => new Array(3).fill(0));
   let len = moves.length;
   for(let i = 0; i < len; i++){
     // 如果是A下棋
@@ -11,7 +11,7 @@ function tictactoe (moves) {
     }
     // 如果下完该步棋产生了获胜者
     if(i >= 4 && chack(arr)){
-      return i % 2 === 0 ? A : B;
+      return i % 2 === 0 ? "A" : "B";
     }
   }
   if(len < 9) return "Pending";
@@ -22,7 +22,7 @@ function chack(arr){
   // 遍历每一种可能获胜的路径（三个点）的下标
   for(let i of compose){
     // 此时i为有三行、每行一个坐标的二维数组（可能获胜路径的下标）
-    if(arr[i[0][0]][i[0][1]] === arr[i[1][0]][i[1][1]] === arr[i[2][0]][i[2][1]]) return true;
+    if(arr[i[0][0]][i[0][1]] === arr[i[1][0]][i[1][1]] && arr[i[1][0]][i[1][1]] === arr[i[2][0]][i[2][1]] && arr[i[2][0]][i[2][1]] !== 0) return true;
   }
   return false;
 }
--- js/test.js
+++ js/test.js
@@ -1,8 +1,9 @@
-console.log(typeof ("1"));
-
-console.log(isNumber('2'));
-function isNumber(n){
-  console.log();
-  console.log(!isNaN(n));
-  return typeof n === 'number' && !isNaN(n);
-}
+// console.log(typeof ("1"));
+//
+// console.log(isNumber('2'));
+// function isNumber(n){
+//   console.log();
+//   console.log(!isNaN(n));
+//   return typeof n === 'number' && !isNaN(n);
+// }
+console.log(new Array(..."ABC"));
