7dc4efddbfff977376f3a8da518c10d84beb3ea0
刘金涛 <tao2500594037@qq.com>
2023-05-27 16:29:26 +0800

    20230527

--- face/myCall.js
+++ face/myCall.js
@@ -1,9 +1,17 @@
+// thisArg为this需指向的对象, 只需把该方法添加到thisArg中this值自然就指向thisArg了
+// 参考链接：https://github.com/mqyqingfeng/Blog/issues/11
 Function.prototype.myCall = function (thisArg, ...args) {
-  const fn = this;
+  // 当传入上下文为null时指向window
+  if (!thisArg) thisArg = window;
+  // 此时this为待执行函数greet
+  const fun = this;
+  // 创建唯一的属性名
   const uniqueKey = Symbol("uniqueKey");
-  thisArg[uniqueKey] = fn;
-
+  // 将函数设为对象的属性
+  thisArg[uniqueKey] = fun;
+  // 执行该函数
   const result = thisArg[uniqueKey](...args);
+  // 删除该函数
   delete thisArg[uniqueKey];
 
   return result;
@@ -15,7 +23,9 @@ const obj = {
 
 // 示例用法
 function greet(greeting, punctuation) {
+  // react
   console.log(`${greeting}, ${this.name}${punctuation}`);
 }
-
+// Hello, jTao!
+// 通过函数（对象）.方法调用，此时方法的this值为该函数（对象）
 greet.myCall(obj, "Hello", "!");
--- face/test.js
+++ face/test.js
@@ -1,6 +1,18 @@
-let timeout;
-console.log(!timeout);
-timeout = setTimeout(() => console.log("MyFunction"), 3000);
-console.log(!timeout);
-clearTimeout(timeout);
-console.log(!timeout);
+// 测试timeout的bool值
+// let timeout;
+// console.log(!timeout);
+// timeout = setTimeout(() => console.log("MyFunction"), 3000);
+// console.log(!timeout);
+// clearTimeout(timeout);
+// console.log(!timeout);
+
+// 测试函数调用原型链中函数方法的this值
+function fun () {
+  let name = "function";
+  function test1 () {
+    let name = "function1";
+    console.log(this.name);
+  }
+  test1();
+}
+fun();
