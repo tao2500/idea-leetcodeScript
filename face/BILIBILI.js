db704d8bb3d5adffbfa6040af4cbd2ffa7c746f8
刘金涛 <tao2500594037@qq.com>
2023-08-30 17:25:01 +0800

    202308030

---
+++ face/BILIBILI.js
@@ -0,0 +1,10 @@
+// 1，勇者打怪可留的最多金币数
+// idea：
+// while (line = readline()) {
+//     var lines = line.split(' ');
+//     var a = parseInt(lines[0]);
+//     var b = parseInt(lines[1]);
+//     print(a + b);
+// }
+
+// 2， 小红刷题 》 = 《
