1b8e1a1e6fa59673e820623838afa801c9276a10
刘金涛 <tao2500594037@qq.com>
2023-10-15 20:55:27 +0800

    20231015

---
+++ face/CVTEBefore.js
@@ -0,0 +1,13 @@
+// setTimeout()传参
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function(i) {
+//     console.log(i);
+//   }, 1000, i);
+// }
+
+// // 函数的提升高于变量的提升
+// var a;
+// function a() {
+//   console.log(1);
+// }
+// console.log(a);
